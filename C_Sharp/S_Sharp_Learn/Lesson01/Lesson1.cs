045eedcf91cc27115fa0ee1846d877399af5fee6
Александр <@LAPTOP-N5BHSA15>
2024-02-28 22:05:03 +0300

    test

---
+++ C_Sharp/S_Sharp_Learn/Lesson01/Lesson1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.S_Sharp_Learn.Lesson01
+{
+    internal class Lesson1
+    {
+    }
+}
---
+++ C_Sharp/S_Sharp_Learn/Lesson15/Lecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.S_Sharp_Learn.Lesson15
+{
+    internal class Lecture
+    {
+    }
+}
