e5add64555a103bc5fdd6b11eae36a9573499e41
Alexandr <a.a.shapovov@bk.ru>
2024-02-28 13:27:38 +0300

    Test

---
+++ C_Sharp/Relationships/Aggregation/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Relationships.Aggregation
+{
+    public abstract class Engine
+    { }
+    public class Car
+    {
+        Engine engine;
+        public Car(Engine eng)
+        {
+            engine = eng;
+        }
+    }
+
+}
